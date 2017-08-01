4420d3e1a04060a74f9f76a3c03e1291852db4b6
Reno McKenzie <renomckenzie@Renos-MacBook-Pro.local>
2017-08-01 12:32:45 -0700

    added counter to pullArticles.js

--- pullArticles.js
+++ pullArticles.js
@@ -1,16 +1,11 @@
-const async = require("async");
-const path = require("path");
-const download = require("download");
-const realFs = require("fs");
-let fs = require("graceful-fs");
+const async = require('async');
+const path = require('path');
+const download = require('download');
+const realFs = require('fs');
+let fs = require('graceful-fs');
 fs.gracefulify(realFs);
-const { loadListFile } = require("./_helper");
-const {
-  CONCURRENT_CONNECTIONS,
-  WIKI_DL,
-  MEDIA_WIKI,
-  WIKI_LIST
-} = require("./config");
+const { loadListFile } = require('./_helper');
+const { CONCURRENT_CONNECTIONS, WIKI_DL, MEDIA_WIKI, WIKI_LIST } = require('./config');
 
 /* --- Notes about imports --- 
 I am using the module graceful-fs which patches fs to fix problems with 
@@ -25,10 +20,11 @@ function downloadWikiArticles(articleListArr) {
   function processArticle(article, callback) {
     /* Check for file access. If the process returns an error, this means
     the file doesn't exist. So lets download the article! Otherwise, skip */
-    const filePath = path.join(WIKI_DL, article + ".html");
+    logCounter++;
+    const filePath = path.join(WIKI_DL, article + '.html');
     fs.access(filePath, fs.constants.R_OK, err => {
-      if (err) console.log("downloading", article);
-      else console.log("skipping", article);
+      if (err) console.log(`${logCounter}/${articleListArr.length} | downloading ${article}`);
+      else console.log('skipping', article);
       err ? getArticle(article, callback) : callback();
     });
   }
@@ -36,32 +32,31 @@ function downloadWikiArticles(articleListArr) {
   // Do the actual downloading
   function getArticle(article, callback) {
     const url = MEDIA_WIKI + article;
-    const filePath = path.join(WIKI_DL, article + ".html");
-    download(url)
-      .then(html => fs.writeFile(filePath, html, callback))
-      .catch(err => {
-        // Push article back on the queue if it is a 429 (too many requests)
-        if (err.statusCode === 429) {
-          console.log("pushing", article, "back on the queue");
-          queue.push(article);
-        }
-        console.log("   ", err.statusCode, article);
+    const filePath = path.join(WIKI_DL, article + '.html');
+    download(url).then(html => fs.writeFile(filePath, html, callback)).catch(err => {
+      // Push article back on the queue if it is a 429 (too many requests)
+      if (err.statusCode === 429) {
+        console.log('pushing', article, 'back on the queue');
+        queue.push(article);
+      }
+      console.log('   ', err.statusCode, article);
 
-        // Write error out to file
-        const ERR_FILE = path.join(__dirname, "missing_articles.txt");
-        fs.appendFile(ERR_FILE, `${err.statusCode} ${article}\n`, err => {
-          if (err) console.log("problems appending to error file", err);
-          callback();
-        });
+      // Write error out to file
+      const ERR_FILE = path.join(__dirname, 'missing_articles.txt');
+      fs.appendFile(ERR_FILE, `${err.statusCode} ${article}\n`, err => {
+        if (err) console.log('problems appending to error file', err);
+        callback();
       });
+    });
   }
 
   /* Initialize a queue that limits concurrent downloads. The queue will 
   automatically start processing. Upon finish, the drain function will run */
+  let logCounter = 0;
   const queue = async.queue(processArticle, CONCURRENT_CONNECTIONS);
   queue.push(articleListArr);
   queue.drain = () => {
-    console.log("All articles downloaded");
+    console.log('All articles downloaded');
   };
 }
 
