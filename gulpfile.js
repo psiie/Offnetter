55e427036156d32359a6011a5fe4bd92bad61630
darkenvy <darkenvy6@gmail.com>
2017-08-02 20:56:01 -0700

    html fixing works in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ const gulp = require("gulp");
 const gs = require("gulp-selectors");
 const cleanCSS = require("gulp-clean-css");
 const htmlmin = require("gulp-html-minifier");
+const htmltidy = require("gulp-htmltidy");
+
+const tidyOpts = {
+  doctype: "html5",
+  hideComments: true,
+  indent: false
+};
 
 const cleanCssOpts = {
   compatibility: "ie7",
@@ -39,17 +46,29 @@ gulp.task("minify-css", () => {
     .pipe(gulp.dest("preprocessed_wiki_articles/index_clean")); //
 });
 
-gulp.task("htmlmin", function() {
+gulp.task("htmlmin", () => {
+  console.log(
+    "Warning. You will get no status on this task. It will take a long time"
+  );
   return gulp
     .src([
       "preprocessed_wiki_articles/index_clean/index.css",
       "preprocessed_wiki_articles/**/*.html"
     ]) //
+    .pipe(htmltidy(tidyOpts)) //
     .pipe(htmlmin(htmlMinOpts)) //
     .pipe(gs.run()) //
     .pipe(gulp.dest("postprocessed_wiki_articles")); //
 });
 
+// ----------------------------------------------------------------- //
+
+gulp.task('move-css' () => {
+
+})
+
+// gulp.task("default", ["test"]);
 // gulp.task("default", ["minify-css"]);
 // gulp.task("default", ["htmlmin"]);
 gulp.task("default", ["minify-css", "htmlmin"]);
+gulp.task("nooptim", [""])
