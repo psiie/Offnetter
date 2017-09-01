071b7d5478992436c247c009247fab4a4aaabb42
Reno McKenzie <reno.mckenzie@ticketmaster.com>
2017-08-31 22:10:33 -0700

    new index page powered by angular

--- prepare_zimwriterfs.sh
+++ prepare_zimwriterfs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cp favicon.ico processed_wiki_articles/
 cp index.css processed_wiki_articles/
-echo "Done Copying favicon.ico and index.css into processed_wiki_articles folder"
+cp angular.min.js processed_wiki_articles/
+cp index.html processed_wiki_articles/
+echo "Done Copying favicon html css and angular into processed_wiki_articles folder"
