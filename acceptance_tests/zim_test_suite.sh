44fa182e1465e787bc59f8eb1386b19d0706684b
darkenvy <darkenvy6@gmail.com>
2017-10-27 19:58:37 -0700

    made a small comment

--- acceptance_tests/zim_test_suite.sh
+++ acceptance_tests/zim_test_suite.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+# Run this script with two parameters. Zimfile.zim and article_list.txt
+
 # Check if Root. If not, quit.
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-# "2017-10-25_0218.zim"
-
-# IMG="output/" + $1
 exec docker run -v $(pwd)/output:/output -p 8009:8009 darkenvy/kiwix-serve ./kiwix-serve --port=8009 /output/$1 &
 
 echo 'Waiting 5 seconds for container to boot'
