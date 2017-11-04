f0ad5fbf532afa2b74994410bbc006e47b906032
darkenvy <darkenvy6@gmail.com>
2017-11-04 13:44:16 -0700

    added notes to create_zim.sh

--- create_zim.sh
+++ create_zim.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Check if Root. If not, quit.
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Change settings below
+# Be sure to escape ALL quotemarks. event singlequotes
+# This is due to "$@" or "$1" not preserving quotes in shell scripts
+
 ./zimwriterfs \
 \
 --language=fr \
